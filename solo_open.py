ce3b8fee1e5d030caba83910409ad52e408c6439
raqblanc <33464889+raqblanc@users.noreply.github.com>
2018-10-13 20:55:40 +0200

    rawT.csv -> raw_open.csv
    De la tabla traspuesta nos quedamos solo con los valores de apertura

---
+++ solo_open.py
@@ -0,0 +1,21 @@
+import csv
+archivoEntrada = open("rawT.csv")
+entrada = csv.reader(archivoEntrada, delimiter=",")
+archivoSalida=open("raw_open.csv","w")
+salida=csv.writer(archivoSalida, delimiter=',')
+
+solo_open = []
+j = 0 #Fila
+for row in entrada:
+    i = 0 #Columna
+    solo_open.append([])
+    for dato in row:
+        if (i == 0 or i == 1 or ((i+2) % 4 == 0)): #Es la columna open (+2 por date y date_pretty)
+            solo_open[j].append(dato)
+        i = i + 1
+    j = j + 1
+for row in solo_open:
+    salida.writerow(row)
+
+archivoEntrada.close()
+archivoSalida.close()
