1ba78737d897f9b0101a3dc3fd3c23b01253189e
Raquel Blanco Morago <33464889+raqblanc@users.noreply.github.com>
2018-11-25 16:02:24 +0100

    Nombre archivo salida
    Ahora en vez de entrar escalado_close y salir escalado_close_fc sale close_fc

--- quita_fc.py
+++ quita_fc.py
@@ -6,7 +6,8 @@ fichero=sys.argv[1]
 archivoEntrada = open(fichero)
 entrada = csv.reader(archivoEntrada, delimiter=",")
 ss=fichero.split('.')
-archivoSalida=open(ss[0]+'_fc.csv',"w")
+sa=ss[0].split('_')
+archivoSalida=open(sa[1]+'_fc.csv',"w")
 salida=csv.writer(archivoSalida, delimiter=',',lineterminator='\n')
 
 nw=[]
@@ -16,14 +17,14 @@ for row in entrada:
 
 	row.pop(0)
 	row.pop(0)
-	
+
 	nw.append(row)
 	# borro las columnas de date y date_pretty
 	rown+=1
 
-		
+
 for row in nw:
-	
+
     salida.writerow(row)
 
 archivoEntrada.close()
