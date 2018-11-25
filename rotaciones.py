dc31e172a9abb9f80453118217a2517984135ffe
Raquel Blanco Morago <33464889+raqblanc@users.noreply.github.com>
2018-11-25 19:09:08 +0100

    Bucles columna, futuro y pasado
    Para {open_close,volatilidad,volumen} NOOOO HECHO
    	Para cada columna i: HECHO
                 Para cada futuro f in (1,90): HECHO
                        Para cada pasado in (2..90): HECHO
                                     Generar Fichero variable_ci_futuro_pasado.csv HECHO
    
    Además, ahora se escriben las cabeceras y quitamos la primera columna.
    El código que calcula las rotaciones está tal cual.
    El rango ahora mismo está solo con futuro 6 pasado 5 (como prueba).
    He comprobado los resultados con lo que tu subiste de c0 y los datos están igual.

--- rotaciones.py
+++ rotaciones.py
@@ -1,78 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[35]:
-
-
 import csv
 import sys
-fichero = sys.argv[1]
-
+fichero = sys.argv[1] #Por ejemplo open_fc.csv
 archivoEntrada = open(fichero)
 entrada = csv.reader(archivoEntrada,delimiter=",")
-
 ss=fichero.split('.')
-archivoSalida=open(ss[0]+'_rotado.csv',"w")
-salida=csv.writer(archivoSalida,lineterminator='\n', delimiter=',')
+archivosSalida = []
 
 
 results = []
+i = 0
 for lista in entrada: # Cada fila es una lista
-    results.append(lista)
-        
-futuro_a_predecir=11
-pasado_a_utilizar=4
+    if (i != 0): #La primera no porque son las cabeceras
+        results.append(lista)
+    else:
+        headers = lista
+    i+=1
 
-print(results)
-
-
-columna_predecir=1
-
-fila =0 #fila que voy metiendo en el nuevo
-dia_actual=0 #dia por el que voy del viejo
-final=[]
-for row in results:
-    if(dia_actual>=futuro_a_predecir+pasado_a_utilizar-1):#empieza apartir de la fila 11
-        
-        final.append([])
-        col =0
-        for dato in row:
-            if col==0:
-                final[fila].append(dia_actual)
-            elif col==columna_predecir:
-                
-                dia=dato
-                pasado=results[dia_actual-futuro_a_predecir][col]
-               
-                
-                final[fila].append(float(dia)-float(pasado))
-            else:
-				final[fila].append(float(results[dia_actual-futuro_a_predecir][col])-float(results[dia_actual-futuro_a_predecir-pasado_a_utilizar+1][col]))
-                
-                
-            col+=1
-        
-        
-        fila+=1
-    
-    dia_actual+=1
-    
-for fila in final:
-    salida.writerow(fila)
+tam = len(headers)
+for columna_predecir in range(0, tam):
+    for futuro_a_predecir in range(6,7): #range(1,91)
+        for pasado_a_utilizar in range(5,6): #range(2,90)
+            fila =0 #fila que voy metiendo en el nuevo
+            dia_actual=0 #dia por el que voy del viejo
+            final=[]
+            for row in results:
+                if(dia_actual>=futuro_a_predecir+pasado_a_utilizar-1):#empieza a partir de la fila 11
+                    final.append([])
+                    col = 0
+                    for dato in row:
+                        if col==columna_predecir:
+                            dia=dato
+                            pasado=results[dia_actual-futuro_a_predecir][col]
+                            final[fila].append(float(dia)-float(pasado))
+                        else:
+            				final[fila].append(float(results[dia_actual-futuro_a_predecir][col])-float(results[dia_actual-futuro_a_predecir-pasado_a_utilizar+1][col]))
+                        col+=1
+                    fila+=1
+                dia_actual+=1
+            name = 'open_'+ 'c'+ str(columna_predecir)+'_'+str(futuro_a_predecir)+'_'+str(pasado_a_utilizar)
+            archivoSalida=open(name+'.csv',"w")
+            salida=csv.writer(archivoSalida,lineterminator='\n', delimiter=',')
+            salida.writerow(headers)
+            for fila in final:
+                salida.writerow(fila)
+            archivoSalida.close()
 
 archivoEntrada.close()
-archivoSalida.close()
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
