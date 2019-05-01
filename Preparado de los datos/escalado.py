7820068d62bccfb4a532353d681a6b4c0463394e
celiia <38191203+celiia@users.noreply.github.com>
2019-05-01 15:15:55 +0200

    Cambio carpeta

---
+++ Preparado de los datos/escalado.py
@@ -0,0 +1,56 @@
+# 1. Recorro la tabla para encontrar minsimo y maxsimo de cada columna (usando una lista de maxs y una de mins)
+# 2. Recorro la tabla para escribir en el nuevo csv el dato escalado
+
+import csv
+import sys
+archivoEntrada = open(sys.argv[1])
+entrada = csv.reader(archivoEntrada, delimiter=",")
+archivoSalida=open(sys.argv[2],"w")
+salida=csv.writer(archivoSalida, delimiter=',')
+
+# 1
+maxs = []
+mins = []
+i = 0 # Fila
+for row in entrada:
+    j = 0 # Columna
+    if (i != 0): # La primera fila no interesa
+        for dato in row:
+            if (j > 1): #Date y Date_Pretty no
+                if (i == 1): # Guardo inicialmente el valor de la primera fila
+                    maxs.append(float(dato))
+                    mins.append(float(dato))
+                else: # Recorro y actualizo maxs y mins
+                    if (maxs[j-2] < float(dato)):
+                        maxs[j-2] = float(dato)
+                    if (mins[j-2] > float(dato)):
+                        mins[j-2] = float(dato)
+            j = j + 1
+    i = i + 1
+
+escalado = []
+# 2
+archivoEntrada.seek(0) #Volver al comienzo del csv
+i = 0 # Fila
+for row in entrada:
+    j = 0 # Columna
+    escalado.append([])
+    for dato in row:
+        if (i == 0): # Fila de titulos
+            escalado[i].append(dato)
+        else:
+            if (j < 2): #Date y Date_Pretty
+                escalado[i].append(dato)
+            else:
+                valor = float (dato)
+                minc =  float (mins[j-2])
+                maxc =  float (maxs[j-2])
+                escalado[i].append(((valor - minc)/(maxc - minc))) # j - 2 por las dos primeras cols
+        j = j + 1
+    i = i + 1
+
+for row in escalado:
+    salida.writerow(row)
+
+archivoEntrada.close()
+archivoSalida.close()
---
+++ Preparado de los datos/missing_v.py
@@ -0,0 +1,32 @@
+import csv
+import sys
+archivoEntrada = open(sys.argv[1])
+entrada = csv.reader(archivoEntrada, delimiter=",")
+archivoSalida=open(sys.argv[2],"w")
+salida=csv.writer(archivoSalida, delimiter=',')
+
+i = 0 #Fila
+rawt = []
+no_vale = [] # Tiene las posiciones de las columnas que no valen
+for row in entrada:
+    no_vale.append([])
+    rawt.append([])
+    j = 0 #Columna
+    if (i == 0): #La fila de los titulos
+        for dato in row:
+            if (dato.startswith("EUR003M") or dato.startswith("EUR006M") or dato.startswith("EUR012M") or dato.startswith("CB3_Govt") or dato.startswith("CB6_Govt") or dato.startswith("CB12_Govt") or dato.startswith("ECCPEMUY")):
+                no_vale.append(j) #Guardo las posiciones de las columnas que hay que borrar en cada fila
+            else: #Si no es uno de esos, lo escribo                
+                rawt[0].append(dato)
+            j = j + 1
+    else: #El resto de filas
+        for dato in row:
+            if (j not in no_vale): #Esa pos no es de una columna eliminada
+                rawt[i].append(dato)
+            j = j + 1
+    i = i + 1
+for row in rawt:
+    salida.writerow(row)
+
+archivoEntrada.close()
+archivoSalida.close()
